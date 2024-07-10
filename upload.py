15e562e8a5c1871a6f20093ec4a20a1bafa1247a
Diego Costa <diego.costa@ciag.org.br>
2024-07-10 20:28:46 -0300

    feat: all excel

--- upload.py
+++ upload.py
@@ -7,8 +7,7 @@ from pymongo.server_api import ServerApi
 import os
 from flasgger import Swagger
 
-app = Flask(__name__)
-swagger = Swagger(app)
+
 
 URI = os.getenv('MONGODB_URI')
 
@@ -22,48 +21,48 @@ def save_object_to_json(data, file_path):
 
 def process_excel(file_path):
     try:
-        workbook = openpyxl.load_workbook(file_path, data_only=True)  # Carrega o arquivo Excel com valores calculados
-        sheet = workbook.active  # Obtém a primeira planilha ativa
+        workbook = openpyxl.load_workbook(file_path, data_only=True)
+        all_sheets_data = {}
 
-        # Monta os dados para enviar para a API
-        data = {
-            "Granja": sheet['B1'].value if sheet['B1'].value else '',
-            "Genética": sheet['B2'].value if sheet['B2'].value else '',
-            "Data Nasc.": sheet['B3'].value.strftime('%Y-%m-%d') if isinstance(sheet['B3'].value, datetime) else '',
-            "Lote": sheet['B4'].value if sheet['B4'].value else '',
-            "Quant. Inicial Aves": sheet['G1'].value if sheet['G1'].value else '',
-            "Galpão Cria": sheet['G2'].value if sheet['G2'].value else '',
-            "Galpão Recria": sheet['G3'].value if sheet['G3'].value else '',
-            "Galpão Postura": sheet['G4'].value if sheet['G4'].value else '',
-            "Cria/Recria": sheet['M1'].value if sheet['M1'].value else '',
-            "Postura": sheet['M2'].value if sheet['M2'].value else '',
-            "N1": sheet['N1'].value if sheet['N1'].value else '',
-            "N2": sheet['N2'].value if sheet['N2'].value else ''
-        }
+        for sheet in workbook.worksheets:
+            sheet_data = {
+                "Granja": sheet['B1'].value if sheet['B1'].value else '',
+                "Genética": sheet['B2'].value if sheet['B2'].value else '',
+                "Data Nasc.": sheet['B3'].value.strftime('%Y-%m-%d') if isinstance(sheet['B3'].value, datetime) else '',
+                "Lote": sheet['B4'].value if sheet['B4'].value else '',
+                "Quant. Inicial Aves": sheet['G1'].value if sheet['G1'].value else '',
+                "Galpão Cria": sheet['G2'].value if sheet['G2'].value else '',
+                "Galpão Recria": sheet['G3'].value if sheet['G3'].value else '',
+                "Galpão Postura": sheet['G4'].value if sheet['G4'].value else '',
+                "Cria/Recria": sheet['M1'].value if sheet['M1'].value else '',
+                "Postura": sheet['M2'].value if sheet['M2'].value else '',
+                "N1": sheet['N1'].value if sheet['N1'].value else '',
+                "N2": sheet['N2'].value if sheet['N2'].value else ''
+            }
 
-        tabela_dados = []
-        for row in sheet.iter_rows(min_row=7, values_only=True):
-            if any(row):  # Verifica se a linha não está vazia
-                tabela_dados.append({
-                    "Data": row[0].strftime('%Y-%m-%d') if isinstance(row[0], datetime) else '',
-                    "Semana": row[1] if row[1] else '',
-                    "Dia": row[2] if row[2] else '',
-                    "Aves Mortas": row[3] if row[3] else '',
-                    "Previsto %": row[4] if row[4] else '',
-                    "Previsto Aves": row[5] if row[5] else '',
-                    "Real Aves": row[6] if row[6] else '',
-                    "Previsto % Ovos": row[7] if row[7] else '',
-                    "Previsto Qtde Ovos": row[8] if row[8] else '',
-                    "Real Ovos": row[9] if row[9] else '',
-                    "Real % Ovos": row[10] if row[10] else '',
-                    "Previsto Ração": row[11] if row[11] else '',
-                    "Real Ração": row[12] if row[12] else ''
-                })
+            tabela_dados = []
+            for row in sheet.iter_rows(min_row=7, values_only=True):
+                if any(row):
+                    tabela_dados.append({
+                        "Data": row[0].strftime('%Y-%m-%d') if isinstance(row[0], datetime) else '',
+                        "Semana": row[1] if row[1] else '',
+                        "Dia": row[2] if row[2] else '',
+                        "Aves Mortas": row[3] if row[3] else '',
+                        "Previsto %": row[4] if row[4] else '',
+                        "Previsto Aves": row[5] if row[5] else '',
+                        "Real Aves": row[6] if row[6] else '',
+                        "Previsto % Ovos": row[7] if row[7] else '',
+                        "Previsto Qtde Ovos": row[8] if row[8] else '',
+                        "Real Ovos": row[9] if row[9] else '',
+                        "Real % Ovos": row[10] if row[10] else '',
+                        "Previsto Ração": row[11] if row[11] else '',
+                        "Real Ração": row[12] if row[12] else ''
+                    })
 
-        # Adiciona a lista de dados à estrutura JSON
-        data["Tabela"] = tabela_dados
+            sheet_data["Tabela"] = tabela_dados
+            all_sheets_data[sheet.title] = sheet_data
 
-        return data
+        return all_sheets_data
 
     except Exception as e:
         print(f'Erro ao processar o arquivo Excel: {e}')
@@ -80,15 +79,19 @@ def save_to_mongodb(data):
             print(e)
             return False
 
-        db = client['Artabas']  # Substitua pelo nome do seu banco de dados
-        collection = db['Aplicativo']  # Substitua pelo nome da sua coleção
+        db = client['Artabas']  
+        collection = db['Aplicativo'] 
 
-        collection.insert_one(data)
+        for sheet_name, sheet_data in data.items():
+            collection.insert_one({"Sheet": sheet_name, "Data": sheet_data})
+
         print('Dados inseridos com sucesso no MongoDB')
         return True
 
     except Exception as e:
         print(f'Erro ao salvar os dados no MongoDB: {e}')
         return False
+
+
 
 
